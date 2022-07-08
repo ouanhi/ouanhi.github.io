fd9469914fb760e64f64e28fec1442f189569aa9
ouanhi <kekekele1112@gmail.com>
2022-07-08 13:09:11 +0800

    Site updated: 2022-07-08 13:09:09

---
+++ dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: 'MELANCHOLY',
+        artist: 'White Cherry',
+        url: 'https://lc-gluttony.s3.amazonaws.com/fPK6JAm6DUYg/xELGtab4ikWieddvtttLdxV9a36urApu/MELANCHOLY%20-%20White%20Cherry%20.mp3',
+        cover: 'https://lc-gluttony.s3.amazonaws.com/fPK6JAm6DUYg/ql7jqRHjLtKeiA1Dq0m0lvN1AmkoB2TG/109951165424074168.jpg',
+      }
+    ]
+});
